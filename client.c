fad7d96541d4234bb03e417c64314fe5416ad427
KorotkovBS <only77nt@gmail.com>
2018-02-23 18:48:37 +0300

    Version 2.0

--- client.c
+++ client.c
@@ -8,15 +8,23 @@
 #include <stdlib.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <signal.h>
 #include "sock.h"
 
 #define BUFFER_SIZE 255 /*255 символов - стандартный размер буффера + 256 символ - '\0'*/
 #define NAME_SIZE 30 /*30 символов вполне достаточно для никнейма*/
 
-int main() 
+int sockfd;
+
+void handler(int sig) /*Функция обработчик сигналов*/
 {
-	int sockfd;
-	int len;
+	close(sockfd);
+	exit(0);
+}
+
+int main(int argc, char *argv[]) 
+{
+	int len,buflen,meslen;
 	struct sockaddr_in address;
 	int result;
 	char buffer[BUFFER_SIZE];
@@ -28,13 +36,17 @@ int main()
 	int server_len, client_len;
 	fd_set readfds, testfds;
 
+	signal(SIGINT,handler);
+	signal(SIGTERM,handler);
+	signal(SIGTSTP,handler);
+
 	memset(&buffer, 0, BUFFER_SIZE); /*Чистим буффер*/	
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0); /*Создаём сокет клиента*/
 
 	address.sin_family = AF_INET; /*Настраиваем его*/
 	address.sin_addr.s_addr = inet_addr("127.0.0.1");
-	address.sin_port = htons(9734);
+	address.sin_port = htons(atoi(argv[1]));
 
 	len = sizeof(address);
 
@@ -48,6 +60,13 @@ int main()
 	printf("Enter your name: "); /*Запоминаем имя клиента*/
 	memset(&Name, 0, BUFFER_SIZE); /*Чистим буффер*/
 	fgets(Name,BUFFER_SIZE-1,stdin);
+	while(!NameCheck(Name))
+	{
+		printf("ERROR! Enter your name again: "); /*Запоминаем имя клиента*/
+		memset(&Name, 0, BUFFER_SIZE); /*Чистим буффер*/
+		fgets(Name,BUFFER_SIZE-1,stdin);
+	}
+	write(sockfd, Name, strlen(Name) - 1);
 	Name[strlen(Name)-1]=':';
 	Name[strlen(Name)]=' ';
 	strcpy(NameCpy,Name);
@@ -65,6 +84,12 @@ while(1)
 			if(FD_ISSET(sockfd,&readfds)) /*Если мы готовы что-то читать*/
 			{
 				read(sockfd, Name, BUFFER_SIZE - 1);
+				if(!str(Name,"/close"))
+				{
+					printf("SERVER WAS CLOSED!\n");
+					close(sockfd);
+					exit(0);
+				}
 				printf("%s\n",Name);
 				memset(&buffer, 0, BUFFER_SIZE); /*Чистим буффер*/
 				memset(&Name, 0, BUFFER_SIZE); /*Чистим буффер*/
@@ -74,9 +99,24 @@ while(1)
 			{
 				memset(&buffer, 0, BUFFER_SIZE); /*Чистим буффер*/
 				fgets(buffer, BUFFER_SIZE - 1, stdin); /*Вводится хорошо*/
+				while(!NameCheck(buffer))
+				{
+					printf("ERROR! Enter your message again: "); /*Запоминаем имя клиента*/
+					memset(&buffer, 0, BUFFER_SIZE); /*Чистим буффер*/
+					fgets(buffer,BUFFER_SIZE-1,stdin);
+				}
+				buflen=strlen(Name)+strlen(buffer);
+				meslen=BUFFER_SIZE-strlen(Name);
+				if(buflen>BUFFER_SIZE)
+				{
+					printf("Very long message! Your message can have %d characters \n", meslen); /*Запоминаем имя клиента*/
+					memset(&buffer, 0, BUFFER_SIZE); /*Чистим буффер*/
+					clean_stdin();
+					continue;
+				}
 				buffer[strlen(buffer)]='\0';
 
-				if(!str(buffer,"/exit") || !str(buffer,"/Exit") || !str(buffer,"/EXIT")) /*Выход*/
+				if((!str(buffer,"/exit") || !str(buffer,"/Exit") || !str(buffer,"/EXIT")) && buffer[6]=='\0') /*Выход*/
 				{
 					printf("Have a good day! :)\n");
 					memset(&buffer, 0, BUFFER_SIZE);
@@ -86,24 +126,23 @@ while(1)
 					exit(0);
 				}
 
-				if(!str(buffer,"/help")) /*Помощь*/
+				if(!str(buffer,"/help") && buffer[6]=='\0') /*Помощь*/
 				{
 					printf("### This chat created by Korotkov Boris, Group 209 © ###\n");
 					printf("### 1) To leave a chat write: /exit ###\n");
 					printf("### 2) To change your nickname write: /nick ###\n");
-					printf("### This command has a bag, the first client of the server can`t do it ###\n");
-					printf("### 3) If you want to know who is online, write: /users ###\n");
-					printf("### 4) To clear screen write: /clear");
+					printf("### 3) To clear screen write: /clear ###\n");
+					printf("### 4) If you want to know the history of server, write: /users ###\n");
 					continue;
 				}
 
-				if(!str(buffer,"/clear")) /*Чистим экран*/
+				if(!str(buffer,"/clear") && buffer[7]=='\0') /*Чистим экран*/
 				{
 					system("clear");
 					continue;
 				}
 
-				if(!str(buffer,"/nick")) /*Меняем никнейм*/
+				if(!str(buffer,"/nick") && buffer[6]=='\0') /*Меняем никнейм*/
 				{
 					write(sockfd, buffer, strlen(buffer) - 1);
 					sleep(2);
@@ -115,6 +154,13 @@ while(1)
 					Name[strlen(Name)-1]=':';
 					Name[strlen(Name)]=' ';
 					strcpy(NameCpy,Name);
+					continue;
+				}
+				
+				if(!str(buffer,"/users") && buffer[7]=='\0') /*Запрашиваем историю (никнеймы входивших когда-либо пользователей)*/
+				{
+					write(sockfd, buffer, strlen(buffer) - 1);
+					printf("### Now online:\n");
 					continue;
 				}
 
