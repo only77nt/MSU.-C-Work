657b7fa53dbd8b529d0bcb5d62fb61393ed7d123
KorotkovBS <only77nt@gmail.com>
2018-02-23 18:52:29 +0300

    Version 2.0

--- sock.h
+++
@@ -1,8 +0,0 @@
-#define BUFFER_SIZE 255 /*255 символов - стандартный размер буффера + 256 символ - '\0'*/
-#define NAME_SIZE 30 /*30 символов вполне достаточно для никнейма*/
-
-struct sockaddr_in server_address;
-struct sockaddr_in client_address;
-struct sockaddr_in address;
-
-int str(char *, char *);
